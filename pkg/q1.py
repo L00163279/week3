3311ddcd0a493fbaf2ddef32af737d0edaa97338
MShafeeq <L00163279@student.lyit.ie>
2021-10-08 11:16:11 +0100

    python variable

--- pkg/q1.py
+++ pkg/q1.py
@@ -34,16 +34,32 @@
 
 if __name__ == "__main__":
 
+    # tuple data
+
     lnumbers = ("L12345" , "L54321")
+
+    #module list data
+
     module_list = ["java_programming" , "python_scripting"]
+
+    student1 = {"L12345":module_list}
+    student2 = {"L54321":module_list}
+
+    print(student1)
+    print(student2)
+
+    # read grades of the student
 
     student1_grade = input("enter student1 grade ")
     student2_grade = input("Enter student2 grade ")
 
     
 
-    student1_details = {student1_grade , lnumbers[0] , module_list[0]}
-    student2_details = {student2_grade , lnumbers[1] , module_list[1]}
+    #creating dictionary for students
+
+
+    student1_details = {lnumbers[0] , student1_grade , module_list[0]}
+    student2_details = {lnumbers[1] , student2_grade , module_list[1]}
 
     print(student1_details , student2_details)
 
