9e320ddf1f4ff0842400e2086208af797e3c7e60
MShafeeq <L00163279@student.lyit.ie>
2021-10-08 10:33:49 +0100

    python variable

--- pkg/q1.py
+++ pkg/q1.py
@@ -9,3 +9,43 @@
 # Listening : 
 
 # ...............................
+
+'''
+
+   Main method of application
+
+
+
+   Linear programming only presented here wrt demo of lists
+
+
+
+   Parameters:
+
+    none
+
+
+
+   Returns:
+
+    none
+
+  '''
+
+if __name__ == "__main__":
+
+    lnumbers = (L12345 , L54321)
+
+    student1 = input("enter student1 number")
+    student2 = input("Enter student2 number")
+
+    student1_details = [student1 , "java_ooprogramming"]
+    student2_details = [student2 , "python_scripting"]
+
+
+
+
+
+
+
+
