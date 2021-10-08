8be85af5944555984ffcf5ddd25799a4747a0c17
MShafeeq <L00163279@student.lyit.ie>
2021-10-08 10:39:13 +0100

    first commit

--- pkg/q1.py
+++ pkg/q1.py
@@ -34,13 +34,14 @@
 
 if __name__ == "__main__":
 
-    lnumbers = (L12345 , L54321)
+    lnumbers = ("L12345" , "L54321")
+    module_list = ["java_programming" , "python_scripting"]
 
     student1 = input("enter student1 number")
     student2 = input("Enter student2 number")
 
-    student1_details = [student1 , "java_ooprogramming"]
-    student2_details = [student2 , "python_scripting"]
+    student1_details = [student1 , lnumbers[0] , module_list[0]]
+    student2_details = [student2 , lnumbers[1] , module_list[1]]
 
 
 
